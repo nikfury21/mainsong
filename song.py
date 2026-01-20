119e39d92722b4461664a5bbd6e1f97c8597843a
nikfury21 <nextgentoolkit21@gmail.com>
2026-01-20 20:24:14 +0530

    Update song.py

--- song.py
+++ song.py
@@ -30,7 +30,8 @@ import requests
 import google.generativeai as genai
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 genai.configure(api_key=GEMINI_API_KEY)
-gemini = genai.GenerativeModel("gemini-2.5-flash")
+gemini = genai.GenerativeModel("gemini-2.0-flash")
+
 
 
 HAS_STREAM_END = hasattr(PyTgCalls, "on_stream_end")
