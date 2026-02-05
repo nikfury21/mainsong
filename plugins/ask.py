f5262add382ebbdc5f6c1859f85b4646c0af9928
nikfury21 <nextgentoolkit21@gmail.com>
2026-02-05 18:18:26 +0530

    Update ask.py

--- plugins/ask.py
+++ plugins/ask.py
@@ -1,9 +1,14 @@
 from pyrogram import Client, filters
 from core.ai_client import ask_ai
 from core.ai_client import ask_groq
+from song import BANNED_USERS
+
 
 @Client.on_message(filters.command("ask") & filters.text)
 async def ask_handler(client, message):
+    if message.from_user.id in BANNED_USERS:
+        return
+
     if len(message.command) < 2:
         await message.reply_text("Say it properly.")
         return
@@ -15,6 +20,10 @@ async def ask_handler(client, message):
 
 @Client.on_message(filters.mentioned & filters.text)
 async def mention_handler(client, message):
+    if message.from_user.id in BANNED_USERS:
+        return
+
+    
     query = message.text.replace("@BestFreakingBot", "").strip()
     if not query:
         query = "Hello"
@@ -25,6 +34,9 @@ async def mention_handler(client, message):
 
 @Client.on_message(filters.text)
 async def name_call_handler(client, message):
+    if message.from_user.id in BANNED_USERS:
+        return
+
     if message.text.startswith("/"):
         return
 
@@ -35,9 +47,13 @@ async def name_call_handler(client, message):
 
 @Client.on_message(filters.reply & filters.text)
 async def reply_handler(client, message):
+    if message.from_user.id in BANNED_USERS:
+        return
+
     replied = message.reply_to_message
 
     if replied and replied.from_user and replied.from_user.id == client.me.id:
         reply = await ask_groq(message.chat.id, message.text)
         await message.reply_text(reply)
+
 
